0249c14f5c543ede745ccba4d17d935a1683cd4f
Samuel Façanha <samufacanha@gmail.com>
2022-08-24 17:06:02 -0300

    feat: login bootstrap

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import Layout from 'components/Layout';
+import Login from 'pages/Login';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import Home from '../pages/Home/';
@@ -7,7 +8,8 @@ const Router: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
         </Route>
